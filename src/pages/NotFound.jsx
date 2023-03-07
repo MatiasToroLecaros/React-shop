6f38905532999f4ceee0148c34bd3e7f68577ff6
Matias <mtorolecaros@gmail.com>
2023-03-07 19:14:22 -0300

    header para todas las rutas v2

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
+
 const NotFound = () => {
-    return (
-        <div>
+    return (        
+        <div>            
             No encontrado, error 404.
         </div>
     );
